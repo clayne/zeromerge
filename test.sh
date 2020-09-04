8d89166505079402eb9365d05c66a0305961e6b1
Jody Bruchon <jody@jodybruchon.com>
2020-09-04 12:38:38 -0400

    Generalize test script a bit

--- test.sh
+++ test.sh
@@ -4,28 +4,32 @@
 
 ERR=0
 
+PROG="./zeromerge"
+
+! $PROG -v && echo "Compile the program first" && exit 1
+
 echo -n "Testing 4K blocks: "
-./zeromerge test/missing1.bin test/missing2.bin test_output.bin
+$PROG test/missing1.bin test/missing2.bin test_output.bin
 if cmp -s test/correct.bin test_output.bin 
 	then echo "PASSED"; ERR=0
 	else echo "FAILED"; ERR=1
 fi
 
 echo -n "Testing 4K w/tail: "
-./zeromerge test/missing1_short.bin test/missing2_short.bin test_output.bin
+$PROG test/missing1_short.bin test/missing2_short.bin test_output.bin
 if cmp -s test/correct_short.bin test_output.bin
 	then echo "PASSED"; ERR=0
 	else echo "FAILED"; ERR=1
 fi
 
 echo -n "Testing mismatch1: "
-if ! ./zeromerge test/missing1.bin test/mismatch.bin test_output.bin 2>/dev/null
+if ! $PROG test/missing1.bin test/mismatch.bin test_output.bin 2>/dev/null
 	then echo "PASSED"; ERR=0
 	else echo "FAILED"; ERR=1
 fi
 
 echo -n "Testing mismatch2: "
-if ! ./zeromerge test/missing1_short.bin test/mismatch_short.bin test_output.bin 2>/dev/null
+if ! $PROG test/missing1_short.bin test/mismatch_short.bin test_output.bin 2>/dev/null
 	then echo "PASSED"; ERR=0
 	else echo "FAILED"; ERR=1
 fi
