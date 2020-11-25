9c8d7ea8f8047171d61cfcd469a8ef10bb079e65
Jody Bruchon <jody@jodybruchon.com>
2020-11-25 12:24:06 -0500

    v1.1.2 release

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 #ifndef ZEROMERGE_VERSION_H
 #define ZEROMERGE_VERSION_H
 
-#define VER "1.1.1"
-#define VERDATE "2020-11-05"
+#define VER "1.1.2"
+#define VERDATE "2020-11-25"
 
 #endif /* ZEROMERGE_VERSION_H */
